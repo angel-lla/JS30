839e98b776fa752f992a80090d443fcbaa3a7487
Angel <angelaguirrel@hotmail.com>
2019-07-23 20:41:55 -0500

    Day 13

---
+++ 13_SlideInOnScroll/app.js
@@ -0,0 +1,39 @@
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    sliderImages.forEach(sliderImage => {
+        console.log('INICIO');
+        // Half way through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        // Bottom of the image
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        console.log(isHalfShown);
+        console.log(isNotScrolledPast);
+        if(isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+
+    });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
